f532c03f143e600b81afdd64a3e98a8df948436f
VImaginator <vimaginator@proton.me>
2020-02-07 09:52:01 +0000

    Update german translation

--- Classification/rgb_d_classification.py
+++ Classification/rgb_d_classification.py
@@ -2,4 +2,10 @@ from keras import applications, optimizers
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
 from keras.models import Sequential, Model 
-from keras.lay
+from keras.layers import convolutional, pooling, core, Input, concatenate
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ProgbarLogger
+
+img_width, img_height = 256, 256
+train_data_dir = "../Videos/rgb-dataset"
+validation_data_dir = "../Videos/rgb-dataset"
+train_data_dir_nir = "../Videos/d-dataset"
