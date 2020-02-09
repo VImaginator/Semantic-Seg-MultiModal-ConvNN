e74f8602c4c73a7de687ddcb923b587304f15654
VImaginator <vimaginator@proton.me>
2020-02-09 07:15:59 +0000

    cpufreq / ACPI: Fix not loading acpi-cpufreq driver regression

--- Classification/rgb_d_classification.py
+++ Classification/rgb_d_classification.py
@@ -9,3 +9,14 @@ img_width, img_height = 256, 256
 train_data_dir = "../Videos/rgb-dataset"
 validation_data_dir = "../Videos/rgb-dataset"
 train_data_dir_nir = "../Videos/d-dataset"
+validation_data_dir_nir = "../Videos/d-dataset"
+nb_train_samples = 207920
+nb_validation_samples = 2079
+batch_size = 10
+epochs = 4
+num_classes = 51
+
+
+inp = Input(shape = (img_width , img_height, 3))
+conv_layer1 = convolutional.Conv2D(8, (3,3), strides=(1, 1), padding='same', activation='relu')(inp)
+conv_layer2 = convol
