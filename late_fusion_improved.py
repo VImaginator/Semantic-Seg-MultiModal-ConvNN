f69665ded774d38165935030546c2b626cc2acdb
VImaginator <vimaginator@proton.me>
2020-08-22 06:19:43 +0000

    backport stable

--- late_fusion_improved.py
+++ late_fusion_improved.py
@@ -2,4 +2,10 @@
 #FOR MODIFYING IMAGES AND ARRAYS
 from datetime import datetime
 import os,cv2
-#from cv2 import getRota
+#from cv2 import getRotationMatrix2D, warpAffine,getAffineTransform,resize,imread,BORDER_REFLECT
+import numpy as np
+#KERAS IMPORTS
+from keras.applications.vgg16 import VGG16
+from keras.callbacks import ProgbarLogger, EarlyStopping, ModelCheckpoint, TensorBoard
+from keras.models import Model, Sequential
+from keras.layer
