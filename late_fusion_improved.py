aa3137eab1c070ca9cca46fafdc65ea5f9e1717b
VImaginator <vimaginator@proton.me>
2021-01-16 20:58:38 +0000

    viafb: don't touch clock state on OLPC XO-1.5

--- late_fusion_improved.py
+++ late_fusion_improved.py
@@ -90,4 +90,11 @@ def data_augmentor(x,state,row_axis=1,col_axis=0,channel_axis=-1):
 #=====================================================================================================================
 #DATAGENERATOR FOR MULTIMODAL SEMANTIC SEGMENTATION
 def Segment_datagen(file_path, rgb_args, nir_args, label_args, batch_size, input_size,val_flag):
-    # Create MEMORY enough for one batch of input(s) + augmentation & labels 
+    # Create MEMORY enough for one batch of input(s) + augmentation & labels + augmentation
+    data = np.zeros((2,batch_size,input_size[0],input_size[1],3), dtype=np.uint8)
+    labels = np.zeros((batch_size,input_size[0]*input_size[1],6), dtype=np.uint8)
+    # Read the file names
+    files = open(file_path)
+    names = files.readlines()
+    files.close()
+    # Enter the indefinite loop of gener
