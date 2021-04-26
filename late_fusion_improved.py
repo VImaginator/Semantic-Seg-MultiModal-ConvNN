96a932ce414150ebca4b9b2d20e3cc23feb5e0ae
VImaginator <vimaginator@proton.me>
2021-04-26 11:09:17 +0000

    tex

--- late_fusion_improved.py
+++ late_fusion_improved.py
@@ -164,4 +164,10 @@ vgg_model_rgb = VGG16(weights='imagenet', include_top = False,modality_num=0)
 conv_model_rgb = vgg_model_rgb(inputs_rgb)
 #conv_model_rgb = Conv2D(32, (3,3), strides=(1, 1), padding = 'same', activation='tanh',data_format="channels_last") (conv_model_rgb)
 #conv_model_rgb = Conv2D(64, (3,3), strides=(1, 1), padding = 'same', activation='tanh',data_format="channels_last") (conv_model_rgb)
-conv_model_rgb = Conv2D(128, (3,3), strides=(1, 1), padding = 'same', activation='tanh',data_format="channels_last") (conv_m
+conv_model_rgb = Conv2D(128, (3,3), strides=(1, 1), padding = 'same', activation='tanh',data_format="channels_last") (conv_model_rgb)
+conv_model_rgb = Conv2D(256, (3,3), strides=(1, 1), padding = 'same', activation='tanh',data_format="channels_last") (conv_model_rgb)
+dropout_rgb = Dropout(0.2)(conv_model_rgb)
+
+# NIR MODALITY BRANCH OF CNN
+inputs_nir = Input(shape=(input_dim[0],input_dim[1],3))
+vg
