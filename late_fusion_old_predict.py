68ae824cdb7fca871c810f892b7791fbd3391342
VImaginator <vimaginator@proton.me>
2021-05-17 22:32:44 +0000

    fix author

--- late_fusion_old_predict.py
+++ late_fusion_old_predict.py
@@ -36,4 +36,9 @@ dropout_rgb = Dropout(0.4)(conv_model_rgb)
 inputs_nir = Input(shape=(input_dim[0],input_dim[1],3))
 vgg_model_nir = VGG16(weights='imagenet', include_top= False,modality_num=1)
 conv_model_nir = vgg_model_nir(inputs_nir)
-conv_model_nir = Conv2D(64, (3,3), strides=(1, 1), padding = 'same', activation='r
+conv_model_nir = Conv2D(64, (3,3), strides=(1, 1), padding = 'same', activation='relu',data_format="channels_last") (conv_model_nir)
+conv_model_nir = Conv2D(128, (3,3), strides=(1, 1), padding = 'same', activation='relu',data_format="channels_last") (conv_model_nir)
+dropout_nir = Dropout(0.4)(conv_model_nir)
+
+# CONACTENATE the ends of RGB & NIR 
+merge_rgb_nir = concatenate([conv_model_nir, conv_model_rgb
