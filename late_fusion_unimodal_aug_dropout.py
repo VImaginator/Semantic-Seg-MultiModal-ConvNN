f814a5e4662585651bd9c8cf8e4f822635202908
VImaginator <vimaginator@proton.me>
2021-10-16 08:32:19 +0000

    modify the readme

--- late_fusion_unimodal_aug_dropout.py
+++ late_fusion_unimodal_aug_dropout.py
@@ -128,4 +128,14 @@ train_RGB_args = gen_args ('/home/krishna/freiburg_forest_dataset/train/rgb/','.
 train_Label_args = gen_args ('/home/krishna/freiburg_forest_dataset/train/GT_color/','.png')
 
 train_generator = Segment_datagen(
-    file_path = '/hom
+    file_path = '/home/krishna/freiburg_forest_dataset/train/train.txt',
+    rgb_args = train_RGB_args,
+    label_args = train_Label_args,
+    batch_size= 1,
+    input_size=input_dim,
+    val_flag = False)
+
+
+
+valid_RGB_args = gen_args ('/home/krishna/freiburg_forest_dataset/valid/rgb/','.jpg')
+valid_Label_args = gen_args ('/home/krishn
