127c735011daa16325e827c9cfcc7df99c9cb0a8
VImaginator <vimaginator@proton.me>
2022-02-25 17:32:54 +0000

    fix publish

---
+++ preprocess/val.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+train_path = '/home/captain_jack/Downloads/freiburg_forest_annotated/train/'
+test_path = '/home/captain_jack/Downloads/freiburg_forest_annotated/test/'
+val_path = '/home/captain_jack/Downloads/freiburg_forest_annotated/valid/'
+
+
+train_dirs = os.listdir(train_path)
+test_dirs = os.
