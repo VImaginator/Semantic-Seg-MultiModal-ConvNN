c35a78727f4d5867bf4819ede816ac5340e2328c
VImaginator <vimaginator@proton.me>
2022-04-08 06:51:43 +0000

    tick

--- preprocess/val.py
+++ preprocess/val.py
@@ -6,4 +6,12 @@ val_path = '/home/captain_jack/Downloads/freiburg_forest_annotated/valid/'
 
 
 train_dirs = os.listdir(train_path)
-test_dirs = os.
+test_dirs = os.listdir(test_path)
+
+
+for folder in train_dirs:
+    os.makedirs('/home/captain_jack/Downloads/freiburg_forest_annotated/valid/'+folder)
+    image_paths = sorted(os.listdir(train_path+folder))
+    for f in range(30):
+        #print len(os.listdir('/home/captain_jack/Downloads/freiburg_forest_annotated/valid/'+folder))
+        shutil.mov
